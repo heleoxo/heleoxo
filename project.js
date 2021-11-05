23f8cd0ba92aef159cebb1bbc48ea865ea1e0e30
heleoxo <93795682+heleoxo@users.noreply.github.com>
2021-11-05 14:33:21 -0600

    Added file project.js
    Contains jquery for card selector

---
+++ project.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    if ($("input[name='lists[stg-upgrade-OXO-tot]']").is(':checked')) {
+        $(".oxoTot").addClass("card-selected");
+        $(".overlay").hide();
+       }
+       else {
+        $(".oxoTot").removeClass("card-selected");
+       }
+
+    $( "input[name='lists[stg-upgrade-OXO-updates]']" ).click(function() {
+        if ($(this).is(':checked')) {
+         $(".oxoUpdates").addClass("card-selected");
+        }
+        else {
+         $(".oxoUpdates").removeClass("card-selected");
+        }
+       });
+
+    $( "input[name='lists[stg-upgrade-OXO-Sales-and-Promotions]']" ).click(function() {
+        if ($(this).is(':checked')) {
+         $(".oxoSales").addClass("card-selected");
+        }
+        else {
+         $(".oxoSales").removeClass("card-selected");
+        }
+       });
+    });
